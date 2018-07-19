6fc6141f3409f3c3973dc6f0d0b73e1653d7b734
Paul <swiergoszp@gmail.com>
2018-07-18 21:01:48 -0600

    tried to increase efficiancy of swiper and html pages

--- javascript/main.js
+++ javascript/main.js
@@ -105,3 +105,24 @@ function GetAge(dateString) {
 
     return age;
 }
+
+// ***************************************************************/
+// initializes swiper on page
+// ***************************************************************/
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+    },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+});
